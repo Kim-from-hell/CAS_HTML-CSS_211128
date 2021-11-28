6fb1592c67342a64c8464b6f3c563afdf4928242
Kim-from-hell <91029648+Kim-from-hell@users.noreply.github.com>
2021-11-28 22:30:08 +0100

    first commit

---
+++ p5/clock.js
@@ -0,0 +1,47 @@
+let myArray= []
+let adding = true
+
+
+  
+function setup() {
+  let canvas = createCanvas(600, 600);
+  canvas.parent ('jup')
+
+  background(0, 0, 0);
+  frameRate();
+}
+
+function draw() {
+  background(0, 0, 0);
+  
+  stroke (255, 255, 255);
+  strokeWeight(2);
+  fill (0,0,0)
+
+
+  let wave= cos(radians(frameCount*2));
+  let posx1= (height/2 + wave*300);
+  
+    myArray.unshift(posx1);
+  
+
+  for (let i = 0; i < 60;i+=1){
+    line (myArray[i], height/2, width/2, 0);
+    line (width/2, height, myArray[i], height/2);
+  }
+  
+console.log(frameCount)
+  
+  if (frameCount>=3600) {
+  noStroke();
+  ellipse (10,10,100,100);
+ } 
+  
+//if (frameCount = 200) {
+ //  console.log(myArray.length)
+ //}  
+  
+
+ 
+ 
+}
